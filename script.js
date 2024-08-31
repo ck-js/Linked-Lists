a16391e9d7fb6646c580c5302d45c58c8d1dbcde
ck-js <ckentertainmentinfo@gmail.com>
2024-08-31 14:48:15 +0700

    add linkedList class methods that appends, prepends node values and adjusts the next pointers of the current node. Also add size method taht returns the number of nodes in our linked list.

--- script.js
+++ script.js
@@ -19,17 +19,27 @@ constructor() {
         }
         current.next = newNode;
 
-
-
-
-    //   let current = this.head;
-    //   while (current.next !== null) {
-    //     current = current.next;
-    //   }
-  
-    //   current.next = newNode;
     }
-  }
+prepend(value) {
+const newNode = new Node(value);
+const currentHead = this.HEAD;
+this.HEAD = newNode;
+newNode.next = currentHead;
+}
+size() {
+    let current = this.HEAD;
+    let count = 0;
+    while (current !== null) {
+        count++;
+        current = current.next;
+    }
+    console.log(count);
+return count;
+}
+
+
+
+  };
 
     // node class containing a value property and link to next node
     class Node {
@@ -42,7 +52,9 @@ constructor() {
 const list1 = new LinkedList();
 list1.append(1);
 list1.append(2);
-list1.append(3);
+list1.prepend(3);
+list1.size()
+
 
 console.log(list1.HEAD); 
 console.log(list1); 
