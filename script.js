1b9031b90cd156c72b6650969c8d8dbea69d7566
ck-js <ckentertainmentinfo@gmail.com>
2024-08-31 23:01:41 +0700

    make progress on linked list class methods of head, tail and at(index). Head method returns the first node, tail method returns the last node and at method returns an array by pushing each node as an array item so that we can call the method and pass in an array index.

--- script.js
+++ script.js
@@ -33,12 +33,35 @@ size() {
         count++;
         current = current.next;
     }
-    console.log(count);
+    
 return count;
 }
+head() {
+if (!this.HEAD) return null;
+    return this.HEAD;
+}
+tail() {
+    if (!this.HEAD) return null;
 
-
-
+    let current = this.HEAD;
+    while (current.next !== null) {
+        current = current.next;
+    }
+    return current;
+    
+}
+at(index) {
+    if (!this.HEAD) return null;
+    
+    let array = [];
+    let current = this.HEAD;
+    while (current) {
+        array.push(current.value);
+        current = current.next;
+    }
+    
+    return array[index];
+}
   };
 
     // node class containing a value property and link to next node
@@ -50,11 +73,16 @@ return count;
       }
     
 const list1 = new LinkedList();
-list1.append(1);
-list1.append(2);
-list1.prepend(3);
+// list1.append(1);
+// list1.append(2);
+// list1.prepend(3);
 list1.size()
+const firstNode = list1.head();
+const lastNode = list1.tail();
+const getByIndex = list1.at(0)
+console.log(getByIndex);
 
 
-console.log(list1.HEAD); 
+// console.log(lastNode);
+// console.log(firstNode);
 console.log(list1); 
